26e6fe0459bb28cea1e2f0dabb448ff2f522ca5e
조현진 <kkadangj@gmail.com>
2020-11-24 22:25:36 +0900

    [ADD] gugudan hooks / Class add

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {GuGudan} from "./lecture/GuGudan";
+import {GuGudanClass} from "./lecture/GuGudanClass";
 
 function App() {
   return (
     <div className="App">
-      <GuGudan/>
+      <GuGudanClass/>
     </div>
   );
 }
